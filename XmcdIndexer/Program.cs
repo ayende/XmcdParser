a327058fbceb560c1f56a267d318f578d1fa3fc6
Ayende Rahien <ayende@ayende.com>
2012-02-10 17:33:58 +0200

    Updates

--- XmcdIndexer/Program.cs
+++ XmcdIndexer/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using ICSharpCode.SharpZipLib.BZip2;
+using ICSharpCode.SharpZipLib.Tar;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Documents;
 using Lucene.Net.Index;
@@ -13,52 +17,87 @@ namespace XmcdIndexer
 {
 	class Program
 	{
-		private const string _indexPath = @"z:\CDDBTestIdx";
+		private const string _indexPath = @"D:\Data\ManualIndex";
 		private static IndexWriter _indexWriter;
 		private static readonly Document ReusableDoc = new Document();
-		private static Parser _parser = new Parser();
 
-		static void Main(string[] args)
+		private static readonly List<Disk> disks = new List<Disk>();
+
+		static void Main()
 		{
 			_indexWriter = new IndexWriter(FSDirectory.Open(new DirectoryInfo(_indexPath)), new StandardAnalyzer(), true,
 			                               IndexWriter.MaxFieldLength.UNLIMITED);
 			_indexWriter.SetUseCompoundFile(false);
 
-			var sw = Stopwatch.StartNew();
+			// TODO: we can do this in batches, and trigger another thread to do that
+			var sw = ParseDisks(disk => disks.Add(disk)); 
+			
+			Console.WriteLine("Elapsed: " + sw.Elapsed);
 
-			var i = 0;
-			const string start = @"Z:\freedb-complete-20120201\rock\42124f16";
-			foreach (var directory in Directory.GetDirectories(@"Z:\freedb-complete-20120201"))
+			sw.Restart();
+
+			foreach (var disk in disks)
 			{
-				foreach (var file in Directory.EnumerateFiles(directory))
-				{
-					if (String.CompareOrdinal(file, start) < 0)
-						continue;
-					try
-					{
-						var disk = _parser.Parse(file);
-						AddDocShortVersion(disk); // TODO: we can do this in batches, and trigger another thread to do that
-
-						if (i++%1000 == 0)
-							Console.Write("\r{0} {1:#,#}           ", file, i);
-					}
-					catch (Exception e)
-					{
-						Console.WriteLine();
-						Console.WriteLine(file);
-						Console.WriteLine(e);
-						_indexWriter.Close();
-						return;
-					}
-				}
+				AddDocShortVersion(disk);
 			}
 
-			sw.Stop();
-			Console.WriteLine("Elapsed: " + sw.Elapsed);
 
 			_indexWriter.SetUseCompoundFile(true);
 			_indexWriter.Optimize();
 			_indexWriter.Close(true);
+
+			Console.WriteLine("Elapsed: " + sw.Elapsed);
+
+		}
+
+		private static Stopwatch ParseDisks(Action<Disk> addToBatch)
+		{
+			int i = 0;
+			var parser = new Parser();
+			var buffer = new byte[1024 * 1024];// more than big enough for all files
+
+			var sp = Stopwatch.StartNew();
+
+			using (var bz2 = new BZip2InputStream(File.Open(@"D:\Data\freedb-complete-20120101.tar.bz2", FileMode.Open)))
+			using (var tar = new TarInputStream(bz2))
+			{
+				TarEntry entry;
+				while ((entry = tar.GetNextEntry()) != null)
+				{
+					if (entry.Size == 0 || entry.Name == "README" || entry.Name == "COPYING")
+						continue;
+					var readSoFar = 0;
+					while (true)
+					{
+						var read = tar.Read(buffer, readSoFar, ((int)entry.Size) - readSoFar);
+						if (read == 0)
+							break;
+
+						readSoFar += read;
+					}
+					// we do it in this fashion to have the stream reader detect the BOM / unicode / other stuff
+					// so we can read the values properly
+					var fileText = new StreamReader(new MemoryStream(buffer, 0, readSoFar)).ReadToEnd();
+					try
+					{
+						var disk = parser.Parse(fileText);
+						addToBatch(disk);
+						if (i++ % 1000 == 0)
+							Console.Write("\r{0} {1:#,#}  {2}         ", entry.Name, i, sp.Elapsed);
+
+						if (i % 50000 == 0)
+							return sp;
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine();
+						Console.WriteLine(entry.Name);
+						Console.WriteLine(e);
+						return sp;
+					}
+				}
+			}
+			return sp;
 		}
 
 		static readonly Field[] fields = new[]
@@ -73,10 +112,12 @@ namespace XmcdIndexer
 			ReusableDoc.GetFields().Clear();
 
 			fields[0].SetValue(disk.Artist);
-			ReusableDoc.Add(fields[0]);
+			if (string.IsNullOrEmpty(disk.Artist) == false)
+				ReusableDoc.Add(fields[0]);
 
 			fields[1].SetValue(disk.Title);
-			ReusableDoc.Add(fields[1]);
+			if(string.IsNullOrEmpty(disk.Title) == false)
+				ReusableDoc.Add(fields[1]);
 
 			fields[2].SetValue(disk.DiskIds.FirstOrDefault() ?? Guid.NewGuid().ToString());
 			ReusableDoc.Add(fields[2]);
--- XmcdParser/Program.cs
+++ XmcdParser/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using ICSharpCode.SharpZipLib.BZip2;
 using ICSharpCode.SharpZipLib.Tar;
 using Raven.Client.Document;
@@ -11,74 +9,82 @@ namespace XmcdParser
 {
 	class Program
 	{
+		private const int BatchSize = 24;
 		static void Main()
 		{
 			using (var store = new DocumentStore
 			{
-				Url = "http://localhost:8080"
+				Url = "https://2.ravenhq.com/databases/ayende-freedb",
+				ApiKey = "2cdd2658-1181-4d67-b15e-7adcc48c461a"
 			}.Initialize())
 			{
 				var session = store.OpenSession();
 				var count = 0;
-				Action<Disk> addToBatch = diskToAdd =>
+
+				var sp = ParseDisks(diskToAdd =>
 				{
 					session.Store(diskToAdd);
 					count += 1;
-					if (count < 512) 
+					if (count < BatchSize) 
 						return;
 
 					session.SaveChanges();
 					session = store.OpenSession();
 					count = 0;
-				};
+				});
 
-				int i = 0;
-				var parser = new Parser();
-				var buffer = new byte[1024*1024];// more than big enough for all files
-
-				var sp = Stopwatch.StartNew();
-
-				using (var bz2 = new BZip2InputStream(File.Open(@"D:\Data\freedb-complete-20120101.tar.bz2", FileMode.Open)))
-				using (var tar = new TarInputStream(bz2))
-				{
-					TarEntry entry;
-					while((entry=tar.GetNextEntry()) != null)
-					{
-						if(entry.Size == 0 || entry.Name == "README" || entry.Name == "COPYING")
-							continue;
-						var readSoFar = 0;
-						while(true)
-						{
-							var read = tar.Read(buffer, readSoFar, ((int) entry.Size) - readSoFar);
-							if (read == 0)
-								break;
-
-							readSoFar += read;
-						}
-						// we do it in this fashion to have the stream reader detect the BOM / unicode / other stuff
-						// so we can read the values properly
-						var fileText = new StreamReader(new MemoryStream(buffer,0, readSoFar)).ReadToEnd();
-						try
-						{
-							var disk = parser.Parse(fileText);
-							addToBatch(disk);
-							if (i++ % 1000 == 0)
-								Console.Write("\r{0} {1:#,#}  {2}         ", entry.Name, i, sp.Elapsed);
-						}
-						catch (Exception e)
-						{
-							Console.WriteLine();
-							Console.WriteLine(entry.Name);
-							Console.WriteLine(e);
-							return;
-						}
-					}
-				}
 				session.SaveChanges();
 
 				Console.WriteLine();
 				Console.WriteLine("Done in {0}", sp.Elapsed);
 			}
+		}
+
+		private static Stopwatch ParseDisks(Action<Disk> addToBatch)
+		{
+			int i = 0;
+			var parser = new Parser();
+			var buffer = new byte[1024*1024];// more than big enough for all files
+
+			var sp = Stopwatch.StartNew();
+
+			using (var bz2 = new BZip2InputStream(File.Open(@"D:\Data\freedb-complete-20120101.tar.bz2", FileMode.Open)))
+			using (var tar = new TarInputStream(bz2))
+			{
+				TarEntry entry;
+				while((entry=tar.GetNextEntry()) != null)
+				{
+					if(entry.Size == 0 || entry.Name == "README" || entry.Name == "COPYING")
+						continue;
+					var readSoFar = 0;
+					while(true)
+					{
+						var read = tar.Read(buffer, readSoFar, ((int) entry.Size) - readSoFar);
+						if (read == 0)
+							break;
+
+						readSoFar += read;
+					}
+					// we do it in this fashion to have the stream reader detect the BOM / unicode / other stuff
+					// so we can read the values properly
+					var fileText = new StreamReader(new MemoryStream(buffer,0, readSoFar)).ReadToEnd();
+					try
+					{
+						var disk = parser.Parse(fileText);
+						addToBatch(disk);
+						if (i++ % BatchSize == 0)
+							Console.Write("\r{0} {1:#,#}  {2}         ", entry.Name, i, sp.Elapsed);
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine();
+						Console.WriteLine(entry.Name);
+						Console.WriteLine(e);
+						return sp;
+					}
+				}
+			}
+			return sp;
 		}
 	}
 }
